70a70f99eeb04d67457e197a2aa4de6bdce99392
Audrey Jacobs <audrey.jacobs1999@gmail.com>
2020-11-03 13:14:32 -0500

    week 9 code assignemnt

--- _tmp/mysql-test/server.js
+++ _tmp/mysql-test/server.js
@@ -1,8 +1,16 @@
 const express = require ('express'); 
 const mysql = require ('mysql'); 
+const SimpleCrypto = require('simple-crypto-js').default; 
+const bodyParser = require('body-parser'); 
 
 const port = process.env.port || 3000; 
 const app = express(); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const key = "testing"; 
+const simpleCrypto = new SimpleCrypto(key); 
 
 var connection = mysql.createConnection({
     host     : 'sql9.freemysqlhosting.net', 
@@ -10,13 +18,26 @@ var connection = mysql.createConnection({
     password : 'NkWCPxnUvd', 
     database : 'sql9373649'
 }); 
-app.get('/api/signup', async (req, res) => {
-    const{username, password} = res.body;
-    const pwd = encryptPassword(password); //write function: encrypt
-    const data = transformDate(new Date());  //write function: transform mysql date format 
+
+function encryptPassword(password){
+    const pass = simpleCrypto.encrypt(password); 
+    return pass; 
+} 
+function transformDate(date){
+    date = date || new Date(); 
+    return date.toISOString().split('T')[0]; 
+}
+
+app.post('/api/signup', (req, res) => {
+    console.log(req.body); 
+    const{username, password} = req.body;
     
-     connection.connect(); 
-    connection.query('INSERT INTO user VALUES ("", ?, ?, date)', [username, password, date], function (error, results, fields){
+    const pwd = encryptPassword(password); //write function: encrypt
+    const date = transformDate(new Date());  //write function: transform mysql date format 
+    console.log(username,pwd);  
+
+    connection.connect(); 
+    connection.query('INSERT INTO user VALUES ("", ?, ?, date)', [username, pwd, date], function (error, results, fields){
         connection.end(); 
         if(error) throw error; 
         res.json(results); 
